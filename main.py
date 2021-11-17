ebc3c3791db4e181d1e7dbdc37809295f4998757
rizkyadiryanto <73731176+Rizky1408@users.noreply.github.com>
2021-11-17 23:03:01 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+# nomor 3
+# bilangan = []
+
+# banyak = int(input("masukan banyak bilangan :"))
+
+# for i in range(banyak):
+#     masukan = int(input("masukan :"))
+#     bilangan.append(masukan)
+
+# Jumlah = sum(bilangan)
+# rata = sum(bilangan) / len(bilangan)
+# print("Total = ", Jumlah)
+# print("Rata-Rata = ", rata)
+
+# x = int(input("x :"))
+# y = int(input("y :"))
+# hasil = pow(x, y)
+# print("Hasil = ", hasil)
+
+# def hitung_faktorial (n):
+#     if n > 2:
+#         return n * hitung_faktorial(n - 1)
+
+#     return 2
+
+# n = int(input("Masaukan Nilai :"))
+# faktorial = hitung_faktorial(n)
+# print(f'{n}! = {faktorial}')
+
+# import random
+
+# tebakan = random.randint(1,10)
+
+# print("=" * 40)
+# print("Silahkan tebak angka")
+# print("=" * 40)
+
+# while True:
+#     jawaban = int(input('\nMasukkan angka: '))
+
+#     if jawaban == tebakan:
+#         print('Selamat, tebakanmu benar!')
+#         break # berhenti paksa
+#     else:
+#         print(
+#        'Tebakanmu terlalu',
+#        'kecil' if jawaban < tebakan else 'besar'
+#     )
+
+
+# x = int(input("x = "))
+# y = int(input("y = "))
+# jml = 0
+# print("\n")
+
+
+# for i in range(x + 1,y):
+#     x += 1
+#     jml += i
+#     print(i," ",end="")
+    
+# print("\nHasil penjumlahan = ",jml)
+
+
